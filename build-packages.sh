6f6a2551d05f0f6a335e0bc76f4098264ddf1c8d
Max Leiter <maxwell.leiter@gmail.com>
2019-12-08 21:22:43 -0800

    remove ds store?

--- build-packages.sh
+++ build-packages.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+find . -name ".DS_Store" -delete
+cd debs
+for f in *; do
+    if [ -d "$f" ]; then
+    	echo $f
+       dpkg-deb -b $f > Packages
+    fi
+done
+cd ..
 if test -f "Packages.bz2"; then
     rm Packages.bz2
 fi
